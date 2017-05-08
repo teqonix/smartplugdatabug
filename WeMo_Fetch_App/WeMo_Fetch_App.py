b6e58de26bbce4fe775520ff954f51d520639856
Travis Roesner <teqonix@live.com>
2017-05-07 23:26:49 -0600

    Added an in-memory sqlite database to hold data returned from the WeMo switches

--- WeMo_Fetch_App/WeMo_Fetch_App.py
+++ WeMo_Fetch_App/WeMo_Fetch_App.py
@@ -1,5 +1,16 @@
 import ouimeaux
+import sqlite3
 from ouimeaux.environment import Environment
+
+db = sqlite3.connect(':memory:')
+
+def init_db(cur):
+    cur.execute('''CREATE TABLE switchDataPoints (
+                        DeviceName TEXT,
+                        Status INTEGER,
+                        EnergyUse INTEGER
+                    )'''
+    )
 
 if __name__ == "__main__":
     print("")
@@ -11,9 +22,12 @@ if __name__ == "__main__":
         env.start()
         env.discover(2)
         #print(env.list_switches())
-        numInterationsBeforeReDiscovery = 7
+        numInterationsBeforeReDiscovery = 3
         loopCount = 0
         switchPowerDataArray = [] #We will store a list of power measurements in this list and then average them before sending them to a flat file or database (we don`t need 300 measurements per minute stored in the database; it should be flattened out)
+
+        cur = db.cursor()
+        init_db(cur)
 
         while loopCount <= numInterationsBeforeReDiscovery:
             for switchStr in ( env.list_switches() ):
@@ -37,7 +51,7 @@ if __name__ == "__main__":
                     #print( currentSwitch.insight_params.values() )
                     currentSwitch.insight.GetInsightParams()
                     insightCurrentPower = currentSwitch.insight_params['currentpower']
-                    switchPowerDataArray.append([currentSwitch.name, insightCurrentPower])
+                    switchPowerDataArray.append([currentSwitch.name, switchCurrentState['BinaryState'], insightCurrentPower])
     #               print("Current switch power: " , switchPowerOutput)
                 exportDataString = ( switchMAC['MacAddr'] + "|" + switchIPAddress + "|" + switchSignalStrength['SignalStrength'] + "|" + switchMAC['SerialNo'] + "|" + switchModelNbr + "|" + switchFirmwareVersion['FirmwareVersion'] + "|" + switchStr + "|" + switchCurrentState['BinaryState'] + "|" + str(insightCurrentPower) )
                 print( exportDataString )
@@ -46,6 +60,16 @@ if __name__ == "__main__":
             print("Current loop count: " + str(loopCount))                
         
         switchPowerDataArray.sort() #We need to sort this list before handing it off the be grouped
+
+        currentPowerSum = 0
+        for powerDataPoint in switchPowerDataArray:
+            currentPowerSum = currentPowerSum + powerDataPoint[2]
+            cur.execute('INSERT INTO switchDataPoints(DeviceName, Status, EnergyUse) VALUES (?,?,?)',powerDataPoint)
+            db.commit()
+            print(powerDataPoint)
+
+        for dataRow in cur.execute('SELECT SUM(EnergyUse) AS SumUsage, DeviceName FROM switchDataPoints GROUP BY DeviceName'):
+            print(dataRow)
 
         input("Press Enter to continue to next switch...")
 
