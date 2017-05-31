775cf46865a65873e30dc52c1e78bfb4085dfdf3
Travis Roesner <teqonix@live.com>
2017-05-30 22:21:20 -0600

    Added SSAS Tabular model and DB objects that aggregate data at the hour level; the Tabular DB seems faster as an SSRS data source than a SQL query; likely because the SSAS DB is in memory

--- WeMo_Fetch_App/ouimeauxDBProject/ouimeauxDBProject/acq/Tables/locations.sql
+++ WeMo_Fetch_App/ouimeauxDBProject/ouimeauxDBProject/acq/Tables/locations.sql
@@ -1,8 +1,10 @@
 ﻿CREATE TABLE [acq].[locations] (
-    [locationSK]        BIGINT         NOT NULL,
+    [locationSK]        BIGINT         CONSTRAINT [DF_locations_locationSK_1] DEFAULT (NEXT VALUE FOR [dbo].[locationSK]) NOT NULL,
     [locationName]      NVARCHAR (255) NOT NULL,
     [locationFloor]     NVARCHAR (255) NOT NULL,
     [locationRoom]      NVARCHAR (255) NOT NULL,
-    [locationAddedDate] DATETIME       NOT NULL
+    [locationAddedDate] DATETIME       CONSTRAINT [DF_locations_locationAddedDate_1] DEFAULT (getdate()) NOT NULL
 );
+
+
 
